da2a32dfbf4d3d1d76e8b462e0aab9217c525b38
kroko_san <acarrasco@gaming.tech>
2024-11-13 17:38:48 +0100

    Changement du nommage de certaines variables et de la fonction de connection (celle-ci renvoie également les infos de base de l'utilisateur)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require("body-parser");
+var cors = require('cors');
 
 // Importation de tous les routers
 const eventRouter = require('./routes/event')
@@ -9,6 +10,7 @@ const userRouter = require('./routes/user')
 
 // Creation de l'app express
 var app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
--- routes/user.js
+++ routes/user.js
@@ -5,11 +5,11 @@ const router = express.Router()
 
 const config = require('../config/connect')
 
-const { generateQuery, isEmpty, emptyParam, testPassword } = require('../functions')
+const { generatequery, isEmpty, emptyParam, testPassword } = require('../functions')
 
 router.get("/", (req, res) => {
     // Si aucun filtre n'est indiqué dans la requette ou que celui-ci est vide ({}) l'API envoie la liste complète des events
-    if(req.body.filter == undefined || req.body.filter == null || isEmpty(req.body.filter))
+    if(req.query.filter == undefined || req.query.filter == null || isEmpty(req.query.filter))
     {
         config.query('SELECT `id`, `nickname`, `isEmploye`, `mail` FROM user', (err, results) => {
             if(err) res.json({"error": err})
@@ -18,7 +18,7 @@ router.get("/", (req, res) => {
         return;
     }
     // Sinon, on récupère la liste des colones de la table event depuis la base de données (permet de modifier la BDD sans toucher a l'API))
-    // Cette requette est faite ici et non dans la fonction generateQuery pour des questions de synchronisation mais également de simplicité
+    // Cette requette est faite ici et non dans la fonction generatequery pour des questions de synchronisation mais également de simplicité
     config.query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'user';", (err, results) => {
         if(err)
         {
@@ -26,7 +26,7 @@ router.get("/", (req, res) => {
             return;
         }
         // Puis on génère une requette SQL avec les paramètres récupérés au dessus  (se )
-        config.query(generateQuery("user", results, req.body.filter, "`id`, `nickname`, `isEmploye`, `mail`"), (err, results) => {
+        config.query(generatequery("user", results, req.query.filter, "`id`, `nickname`, `isEmploye`, `mail`"), (err, results) => {
             if(err) res.json({"error": err})
             res.json(results)
         })
@@ -35,14 +35,15 @@ router.get("/", (req, res) => {
 
 
 router.get("/connect", (req, res) => {
-    if(req.body.params == undefined || req.body.params == null || isEmpty(req.body.params) || req.body.params.nickname == undefined || req.body.params.nickname == null || req.body.params.password == undefined || req.body.params.password == null)
+    console.log(req.query)
+    if(req.query.nickname == undefined || req.query.nickname == null || req.query.password == undefined || req.query.password == null)
     {
         res.json({"error": "must give a nickname and a password"})
         return;
     }
-    const givenPass = req.body.params.password
+    const givenPass = req.query.password
     console.log(givenPass)
-    config.query('SELECT id, pass FROM user WHERE `nickname`=?', [req.body.params.nickname], (err, results) => {
+    config.query('SELECT id, pass, isEMploye, mail FROM user WHERE `nickname`=?', [req.query.nickname], (err, results) => {
         if (err) res.json({err})
         if(results.length != 0)
         {
@@ -53,8 +54,11 @@ router.get("/connect", (req, res) => {
                 givenCryptedPass = response[0].pass
                 if(givenCryptedPass == cryptedPass)
                 {
-                    console.log("c pareil bebe")
-                    res.json({"token": "Un token qui sera généré plus tard"})
+                    require('crypto').randomBytes(48, function(err, buffer) {
+                        var token = buffer.toString('hex');
+                        res.json({"token": token, "user": {"nickname": req.query.nickname}})
+                      });
+                    
                     return;
                 }
                 else res.json({"error": "Incorrect password or username"})
